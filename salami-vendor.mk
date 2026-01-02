724f8670aa3990215590e411915cb4593417c994
Bruno Martins <bgcngm@gmail.com>
2026-01-02 21:52:54 +0000

    salami: Import TFA ADSP modules
    Change-Id: Ibe2bb2f1ee690abaf5208526c48861fea012eb5b

--- salami-vendor.mk
+++ salami-vendor.mk
@@ -1395,6 +1395,8 @@ PRODUCT_PACKAGES += \
     vendor.oplus.hardware.cammidasservice-V1-ndk \
     vendor.oplus.hardware.sendextcamcmd-V1-service-impl \
     vendor.oplus.hardware.sendextcamcmd-V2-ndk \
+    odm_lib_rfsa_adsp_libtfadsp_sb4_0_rx_so \
+    odm_lib_rfsa_adsp_libtfadsp_sb4_0_tx_so \
     EuiccGoogle \
     vendor.qti.camera.aon-impl-1.3.xml \
     vendor.qti.camera.postproc-impl.xml \
